244cc21d81c6b52ac9398430290f448fbf5503d0
Kim Sayno <164379951+KimuSano@users.noreply.github.com>
2025-11-28 09:12:42 +0800

    newInventorySystem
    with Outline and text

---
+++ Assets/Scripts/Objects/HUDController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HUDController : MonoBehaviour
+{
+    public static HUDController instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    [SerializeField] TMP_Text interactionText; // fixed SerializedField -> SerializeField
+
+    public void EnableInteractionText(string text)
+    {
+        interactionText.text = text + " (F)"; // added missing semicolon and space
+        interactionText.gameObject.SetActive(true);
+    }
+
+    public void DisableInteractionText()
+    {
+        interactionText.gameObject.SetActive(false);
+    }
+}
