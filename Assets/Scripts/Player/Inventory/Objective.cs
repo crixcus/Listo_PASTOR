e403292ce93e7711620d367dacccc50de2008058
Kim Sayno <164379951+KimuSano@users.noreply.github.com>
2025-12-15 12:48:56 +0800

    Objective HUD

---
+++ Assets/Scripts/Player/Inventory/Objective.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Objective : MonoBehaviour
+{
+    public TMP_Text objectiveText;
+
+    [Header("Required Items")]
+    public List<Item> requiredItems;
+
+    private Inventory inventory;
+
+    void Start()
+    {
+        inventory = Inventory.instance;
+        UpdateObjectives();
+    }
+
+    void Update()
+    {
+        UpdateObjectives();
+    }
+
+    void UpdateObjectives()
+    {
+        string text = "OBJECTIVES\n";
+
+        foreach (Item item in requiredItems)
+        {
+            if (inventory.items.Contains(item))
+                text += "[X] " + item.itemName + "\n";
+            else
+                text += "[ ] " + item.itemName + "\n";
+        }
+
+        objectiveText.text = text;
+    }
+
+}
