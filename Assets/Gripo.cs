d0f53da5f23976145d7a9ced94c030a34864932f
ctosoc <ctosoc16@gmail.com>
2026-03-29 21:38:52 +0800

    new cutscenes polishing

--- Assets/Gripo.cs
+++ Assets/Gripo.cs
@@ -15,7 +15,7 @@ public class Gripo : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.F))
         {
             TryInteract();
         }
